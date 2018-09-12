901ddaa6b5655a79d9c2cbd1616e84ad2dd7f0a7
Andrej Novikov <jazzaiman@gmail.com>
2018-09-12 18:00:43 +0300

    adding test frameworks
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

---
+++ test/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '../',
+    frameworks: ['browserify', 'mocha', 'chai'],
+
+    browsers: ['PhantomJS'],
+
+    files: [
+      'src/**/*.js',
+      'test/**/*.spec.js',
+    ],
+
+    exclude: [],
+
+    preprocessors: {
+      'src/**/*.js': ['browserify'],
+      'test/**/*.spec.js': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      bundleDelay: 1000,
+      transform: [ ['babelify',  {presets: ["@babel/preset-env"]} ] ],
+      extensions: ['.js']
+    },
+
+    reporters: ['spec'],
+
+    colors: true,
+    logLevel: config.LOG_INFO,
+    singleRun: true
+    // define reporters, port, logLevel, browsers etc.
+  });
+};
