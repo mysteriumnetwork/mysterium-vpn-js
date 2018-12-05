9202da5f1c008a14fa98bbe782be35708d278499
Andrej Novikov <jazzaiman@gmail.com>
2018-12-05 17:18:30 +0200

    add copyright headers and remove unnecessary lint-disable
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/validation.js
+++ src/validation.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 The "mysteriumnetwork/js-tequilapi" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // @flow
 type Property = {
   name: string,
@@ -10,7 +27,6 @@ function validate (typeName: string, obj: Object, property: Property) {
     throw new TypeError(`${typeName}: ${property.name} is not provided`)
   }
 
-  // eslint-disable-next-line
   if (getTypeString(value) !== property.type) {
     throw new TypeError(`${typeName}: ${property.name} should be "${property.type}"`)
   }
