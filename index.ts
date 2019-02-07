052542f9b3538d4a9078ee4f7d18da49bf474f3d
Donatas Kucinskas <donce.lt@gmail.com>
2019-02-07 11:16:22 +0200

    Move QualityCalculator with models from mobile app

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { QualityCalculator } from './src/domain/quality-calculator'
+import { Metrics } from './src/models/metrics'
+import { QualityLevel } from './src/models/quality-level'
+
+export { QualityCalculator, QualityLevel, Metrics }
