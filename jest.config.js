f33ef4e9f36b2f3bbbb91c765840e9f269bba783
Donatas Kucinskas <donce.lt@gmail.com>
2019-02-07 14:34:56 +0200

    Add jest, add specs for QualityCalculator

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.ts$": "ts-jest"
+  },
+  "testRegex": "((\\.|/)spec)\\.ts$",
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
