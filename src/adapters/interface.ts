395fafc18a28f43c118bae8d4d97c319b0997f5b
Donatas Kucinskas <donce.lt@gmail.com>
2019-02-18 14:35:23 +0200

    Add HttpTequilapiClient typescript implementation

--- src/adapters/interface.ts
+++ src/adapters/interface.ts
@@ -21,7 +21,7 @@ type HttpQueryParams = {
 
 interface HttpInterface {
   get (path: string, query?: HttpQueryParams, timeout?: number): Promise<any>
-  post (path: string, data: any, timeout?: number): Promise<any>
+  post (path: string, data?: any, timeout?: number): Promise<any>
   delete (path: string, timeout?: number): Promise<any>
   put (path: string, data: any, timeout?: number): Promise<any>
 }
