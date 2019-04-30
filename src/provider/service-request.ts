8cc003cc90e3a7823765dedbbcc30dbc972d59b3
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-04-30 15:09:51 +0300

    Update copyright project name

--- src/provider/service-request.ts
+++ src/provider/service-request.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The "mysteriumnetwork/js-tequilapi" Authors.
+ * Copyright (C) 2019 The "mysteriumnetwork/mysterium-vpn-js" Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
