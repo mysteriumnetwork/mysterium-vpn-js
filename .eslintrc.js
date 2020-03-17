1a530cacc695ec59f7459c4fb704815a267770cb
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-03-17 21:05:06 +0200

    Check headers as a part of [es]lint process

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,31 @@ module.exports = {
             modules: true,
         }
     },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'prettier/@typescript-eslint'
-    ],
-    plugins: ['prettier', '@typescript-eslint'],
     env: {
         es6: true,
         node: true,
         jest: true
     },
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint'
+    ],
+    plugins: [
+        'prettier',
+        '@typescript-eslint',
+        'header'
+    ],
     rules: {
         '@typescript-eslint/explicit-function-return-type': ['error', {allowExpressions: true}],
+        'header/header': [2, 'block', [
+            '*',
+            {pattern: ' * Copyright \\(c\\) \\d{4} BlockDev AG', template: ' * Copyright (c) 2020 BlockDev AG'},
+            ' *',
+            ' * This source code is licensed under the MIT license found in the',
+            ' * LICENSE file in the root directory of this source tree.',
+            ' '
+        ]]
     },
     overrides: [
         {
