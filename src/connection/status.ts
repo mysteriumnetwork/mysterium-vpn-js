60233a3f09878a14ce6e949d9b15a7dbec971612
Tadas Krivickas <tadas.krivickas@gmail.com>
2022-01-19 10:58:10 +0200

    Add connection status OnHold
    (kill switch activated, session lost)

--- src/connection/status.ts
+++ src/connection/status.ts
@@ -14,6 +14,7 @@ export enum ConnectionStatus {
   NOT_CONNECTED = 'NotConnected',
   DISCONNECTING = 'Disconnecting',
   CONNECTING = 'Connecting',
+  ON_HOLD = 'OnHold',
 }
 
 export interface ConnectionInfo {
